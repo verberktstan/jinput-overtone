084a53ef3c98fc66e90841a5163bb18bf3169403
James Petry <gavilancomun@gmail.com>
2012-04-22 17:25:09 +0100

    lein2

--- project.clj
+++ project.clj
@@ -1,3 +1,11 @@
-(defproject jinput-overtone "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+(defproject jinput-overtone/jinput-overtone "1.0.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/math.numeric-tower "0.0.1"]
+                 [overtone/at-at "1.0.0-SNAPSHOT"]
+                 [overtone/libs.handlers "0.1.1"]
+                 [overtone "0.7.0-SNAPSHOT"]
+                 [jinput "0.0.2"]]
+  :main jinput-overtone.core
+  :min-lein-version "2.0.0"
+  :plugins [[lein-pprint "1.1.1"]]
+  :description "FIXME: write description")
