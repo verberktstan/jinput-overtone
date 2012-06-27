d6352e21d77f664712ab062bd7fed66875f5c1af
James Petry <gavilancomun@gmail.com>
2012-06-27 12:40:43 +0100

    proj

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject jinput-overtone/jinput-overtone "0.0.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/math.numeric-tower "0.0.1"]
-                 [overtone/at-at "1.0.0-SNAPSHOT"]
-                 [overtone/libs.handlers "0.1.1"]
-                 [overtone "0.7.0-SNAPSHOT"]
+                 [overtone/at-at "1.0.0"]
+                 [overtone/libs.handlers "0.2.0"]
+                 [overtone "0.7.0"]
                  [jinput "0.0.2"]]
   :min-lein-version "2.0.0"
   :plugins [[lein-pprint "1.1.1"]]
