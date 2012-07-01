87a38bf9dcc10b84fbee787e16fdb6eaf4932682
James Petry <gavilancomun@gmail.com>
2012-07-01 16:21:12 +0100

    percussion

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject jinput-overtone/jinput-overtone "0.0.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/math.numeric-tower "0.0.1"]
+                 [overtone/midi-clj "0.4.0"]
                  [overtone "0.7.1"]
+                 [midiutils-overtone "0.0.1-SNAPSHOT"]
                  [jinput "0.0.3"]]
   :min-lein-version "2.0.0"
   :plugins [[lein-pprint "1.1.1"]]
