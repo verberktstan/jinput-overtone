a4a6b987c9c6385ec741c8ed9218161d8a7e7f00
James Petry <gavilancomun@gmail.com>
2013-11-18 14:06:27 +0000

    kibit

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
                  [midiutils-overtone "0.0.1-SNAPSHOT"]
                  [jinput "0.0.3"]]
   :min-lein-version "2.0.0"
-  :plugins [[lein-pprint "1.1.1"]]
+  :plugins [[lein-pprint "1.1.1"]
+            [lein-kibit "0.0.8"]]
   :description "Bringing JInput devices to Overtone, the Clojure music project"
   :license {:name "Eclipse Public License 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
